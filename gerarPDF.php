29215db66620219573ab7efa423b59a6fc2f55ab
DevJoaoSales <pmagal539@gmail.com>
2023-05-19 14:14:50 -0300

    update

--- gerarPDF.php
+++ gerarPDF.php
@@ -1,104 +1,40 @@
 <?php
 
-$html = '
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerador PDF</title>
-
-</head>
-<body>
-
-    <div class="container-center">
-
-      <div class="container-btn-mpdf">
-          <a href="gerarPDF.php">Download</a>
-      </div>
-
-        <h1>Teste Página</h1>
-        <p>
+$html = '<h1>Teste Página</h1>';
+$html .= '<p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic debitis minus deserunt accusamus incidunt ipsum voluptatem. Perferendis deserunt ducimus tenetur aliquam, voluptatem sapiente ad ullam, vero nobis reprehenderit assumenda nostrum.<br>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic debitis minus deserunt accusamus incidunt ipsum voluptatem. Perferendis deserunt ducimus tenetur aliquam, voluptatem sapiente ad ullam, vero nobis reprehenderit assumenda nostrum.<br><br>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic debitis minus deserunt accusamus incidunt ipsum voluptatem. Perferendis deserunt ducimus tenetur aliquam, voluptatem sapiente ad ullam, vero nobis reprehenderit assumenda nostrum.<br>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic debitis minus deserunt accusamus incidunt ipsum voluptatem. Perferendis deserunt ducimus tenetur aliquam, voluptatem sapiente ad ullam, vero nobis reprehenderit assumenda nostrum.
         </p>
-
-        <img src="image/street-road.jpg" >
-
-    </div>
-
-<footer>
-
-<style>
-
-  .container-center {
-      margin:100px auto;
-      width:600px;
-  }
-
-  .container-center h1 {
-    text-align:center;
-  }
-
-  .container-center p {
-    font-size:20px;
-    text-align:justify;
-    margin:50px 0px;
-  }
-
-  .container-center img {
-    display:block;
-    margin:20px auto;
-    width:500px;
-    float:inherit;
-  }
-
-  .container-btn-mpdf {
-    margin-top:30px;
-  }
-
-  .container-btn-mpdf a {
-    background:#ccc;
-    padding:5px 20px;
-    color:#000;
-    float:right;
-  }
-
-</style>
-
-</footer>
-
-</body>
-</html>
-
 ';
 
+// IMPORTANTE: Colocar link com URL completa.
+$html .= ' <img src="https://imran.noinsta.com/teste/image/sunday.jpg" >';
 
+/* Carrega a classe DOMPdf */
+require_once("vendor/autoload.php");
 
 
 // Referenciar namespace.
 use Dompdf\Dompdf;
 
-/* Carrega a classe DOMPdf */
-require_once("vendor/autoload.php");
-
 /* Cria a instância */
-$dompdf = new Dompdf();
+// $dompdf = new Dompdf();  // Não suporta Imagens
+$dompdf = new Dompdf(["enable_remote" => true]); // Suporta imagens.
 
 /* Carrega seu HTML */
 $dompdf->loadHtml($html);
 
+// tamanho da folha A4.
+// Formato Paisagem = "landscape" 
+// Formato Retrato = "portrait"
 $dompdf->setPaper('A4','portrait');
 
 /* Renderiza */
 $dompdf->render();
 
-//$dompdf->stream();
-
-/* Exibe */
+/* Gerar PDF */
 $dompdf->stream(
     "Relatorio.pdf", /* Nome do arquivo de saída */
 );
