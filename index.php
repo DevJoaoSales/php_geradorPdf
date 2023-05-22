a6bc758565e5f2d80cbe4a2d8d8ff6cfa713f370
DevJoaoSales <pmagal539@gmail.com>
2023-05-22 14:06:46 -0300

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerador PDF</title>
-    <link rel="stylesheet" href="https://noinsta.com/estilo.css">
+    <link rel="stylesheet" href="https://imran.noinsta.com/teste/estilo.css">
 
 </head>
 <body>
